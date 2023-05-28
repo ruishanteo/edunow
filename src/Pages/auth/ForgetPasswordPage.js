51e2af5b4d85805fc3ab9c9c0be83e85c9d8211d
ruishanteo <ruishan.teo03@gmail.com>
2023-05-29 07:55:56 +0800

    firebase config

---
+++ src/Pages/auth/ForgetPasswordPage.js
@@ -0,0 +1,65 @@
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
+
+import { sendPasswordReset } from "hooks/FirebaseHooks";
+
+export default function ForgetPasswordPage() {
+  const handleReset = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    sendPasswordReset(data.get("email"));
+  };
+
+  return (
+    <Box align="center">
+      <Box
+        sx={{
+          align: "center",
+          backgroundColor: "rgba(255,255,255,0.5)",
+          width: "50vw",
+        }}
+      >
+        <Container component="main" maxWidth="xs">
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Reset Password
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleReset}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+
+              <Button
+                type="submit"
+                onSubmit={handleReset}
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Send Email
+              </Button>
+            </Box>
+          </Box>
+        </Container>
+      </Box>
+    </Box>
+  );
+}
---
+++ src/hooks/FirebaseHooks.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+import { auth } from "./Firebase";
+
+const handleErrorMessage = (err) => {
+  alert(err);
+  console.log("Error Code:", err.code, "Message:", err.message);
+  // throw err;
+};
+
+const logInWithEmailAndPassword = async (email, password) => {
+  return await signInWithEmailAndPassword(auth, email, password).catch(
+    handleErrorMessage
+  );
+};
+
+const registerWithEmailAndPassword = async (name, email, password) => {
+  return await createUserWithEmailAndPassword(auth, email, password)
+    .then(async (response) => {
+      await updateProfile(response.user, { displayName: name });
+    })
+    .catch(handleErrorMessage);
+};
+
+const sendPasswordReset = async (email) => {
+  return await sendPasswordResetEmail(auth, email).catch(handleErrorMessage);
+};
+
+const logout = () => {
+  return signOut(auth);
+};
+
+function useAuth() {
+  const [currentUser, setCurrentUser] = useState();
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => setCurrentUser(user));
+    return unsub;
+  }, []);
+
+  return currentUser;
+}
+
+export {
+  handleErrorMessage,
+  logInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  sendPasswordReset,
+  logout,
+  useAuth,
+};
