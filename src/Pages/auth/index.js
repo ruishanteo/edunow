1c59366328cc2ff239dfcecae7d39af472d70705
ruishanteo <ruishan.teo03@gmail.com>
2023-05-29 07:56:25 +0800

    added page index

---
+++ src/Pages/auth/index.js
@@ -0,0 +1,5 @@
+import ForgetPasswordPage from "./ForgetPasswordPage";
+import LoginPage from "./LoginPage";
+import RegistrationPage from "./RegistrationPage";
+
+export { ForgetPasswordPage, LoginPage, RegistrationPage };
